3c68ccb93e6c9c95d6fc47078da9282fb6589d7f
Laurent Taymans <laurent.taymans@gmail.com>
2024-09-13 19:23:38 +0200

    Uodated requirements.txt

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import spacy
-import PyPDF2
+import pytesseract
+from pdf2image import convert_from_path
+from PIL import Image
 from io import BytesIO
 from docx import Document
 
 # Load the SpaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Function to extract text from a PDF file
+# Function to extract text from images in PDF
 def extract_text_from_pdf(pdf_file):
-    reader = PyPDF2.PdfReader(pdf_file)
+    # Convert PDF to a list of image objects
+    images = convert_from_path(pdf_file)
+    
+    # Initialize an empty string to hold the extracted text
     text = ""
-    for page in range(len(reader.pages)):
-        text += reader.pages[page].extract_text()
+    
+    # Use Pytesseract to extract text from each image
+    for img in images:
+        text += pytesseract.image_to_string(img)
+    
     return text
 
 # Function to anonymize medical data
